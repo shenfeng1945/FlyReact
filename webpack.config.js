e110b8e9bce0f1e0d044b1b36af219c25c190be9
shenfeng1945 <823902826@qq.com>
2019-03-03 23:08:38 +0800

    添加部分webpack配置

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// loader与plugins区别: loader负责把某种文件格式的内容转换成 webpack 可以支持打包的模块,
+// plugin 用于处理更多其他的一些构建任务,模块代码转换的工作由 loader 来处理，除此之外的其他任何工作都可以交由 plugin 来完成。
+module.exports = {
+    entry: './lib/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        // 库名称
+        library: 'flyui',
+        // 将代码封装成什么模块定义。umd统一定义amd和commonJS; if define => amd else if=> commonJs else windows
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' }
+        ]
+    },
+    plugins: [
+        // 将script引入默认的js插入document中,不用手动在index.html写，且文件也不确定。
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
+};
