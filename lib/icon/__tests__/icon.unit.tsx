6cf8ddcedd79e771778452ea324191c51e8bb34e
shenfeng1945 <823902826@qq.com>
2019-03-17 21:53:01 +0800

    添加icon单元测试

--- lib/icon/__tests__/icon.unit.tsx
+++ lib/icon/__tests__/icon.unit.tsx
@@ -1,10 +1,17 @@
-// import Icon from '../icon'
-// import renderer from 'react-test-renderer'
-// import React from 'react'
+import Icon from '../icon'
+import renderer from 'react-test-renderer'
+import React from 'react'
+import { mount } from 'enzyme';
+
 describe('Icon', () => {
-    it('存在', () => {
-        // const json = renderer.create(<Icon />).toJSON()
-        // expect(json).toMatchSnapshot();
-        expect(1).toEqual(1)
+    it('render successfully', () => {
+        const json = renderer.create(<Icon name="qq" />).toJSON()
+        expect(json).toMatchSnapshot();
+    })
+    it('icon click', () => {
+        const fn = jest.fn()
+        const component = mount(<Icon name="qq" onClick={fn} />)
+        component.find('svg').simulate('click');
+        expect(fn).toBeCalled();
     })
 })
