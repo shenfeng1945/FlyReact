a8a6726df4f7803690f194d3db7a57090e435388
shenfeng1945 <823902826@qq.com>
2019-03-27 06:53:33 +0800

    bulid

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,11 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Icon from './icon/icon'
-
-const fn: React.MouseEventHandler<SVGElement> = (e) => {
-  console.log(e.target);
-}
-ReactDOM.render(
-    <Icon name="alipay" className="qq" onClick={fn}></Icon>,
-    document.getElementById('root')
-)
+export { default as Icon } from './icon/icon'
