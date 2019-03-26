08f71b59b13e29c89e6d587271750a94c5228cbb
shenfeng1945 <823902826@qq.com>
2019-03-27 07:34:14 +0800

    添加测试entry

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,6 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const base = require('./webpack.config')
 module.exports = Object.assign({}, base, {
     mode: 'development',
+    // 开发用的
+    entry: {
+        example: './example.tsx'
+    },
     plugins: [
         // 将script引入默认的js插入document中,不用手动在index.html写，且文件也不确定。
         new HtmlWebpackPlugin({
