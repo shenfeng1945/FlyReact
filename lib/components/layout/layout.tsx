b7263d1096c70db774f3b2dd788d21304814f1ac
shenfeng1945 <823902826@qq.com>
2019-04-21 16:53:31 +0800

    重构scopeClass

--- lib/components/layout/layout.tsx
+++ lib/components/layout/layout.tsx
@@ -12,10 +12,10 @@ const Layout: React.FunctionComponent<LayoutProps> = (props) => {
     const {className, ...rest} = props;
     const children = props.children as Array<ReactElement>;
     // 也可以用some
-    const hasAside = children.length &&
-        children.reduce((result,node) => result || node.type === Aside, false);
+    const hasAside = 'length' in children &&
+        children.reduce((result, node) => result || node.type === Aside, false);
     return (
-        <div className={sc('', {extra: [className, hasAside && 'hasAside'].join(' ')})} {...rest}>
+        <div className={sc({'': true, hasAside}, {extra: className})} {...rest}>
             {props.children}
         </div>
     )
