e9777f65b68023a2f95e33e22692b656b31fa6e2
shenfeng1945 <823902826@qq.com>
2019-04-21 17:36:05 +0800

    继续重构scopeClass

--- lib/components/utils/scopeClass.tsx
+++ lib/components/utils/scopeClass.tsx
@@ -9,19 +9,20 @@ interface nameInput {
 
 const scopeClass = (prefix: string) => {
     return (name: string | nameInput, options?: scopeClassOptions) => {
-        let name2;
-        let result;
-        if(typeof name === 'string' || name === undefined){
-            name2 = name;
-            result = [prefix, name2].filter(Boolean).join('-');
-        }else{
-            name2  = Object.entries(name).filter(item => item[1]).map(item => item[0]);
-            result = name2.map(item => [prefix, item].filter(Boolean).join('-')).join(' ')
-        }
+        const namesObject = typeof name === 'string' || name === undefined ?
+            {[name]: name} :
+            name;
+        const scoped = Object.entries(namesObject)
+            .filter(item => item[1] || item[1] === '')
+            .map(item => item[0])
+            .map(item => [prefix, item]
+                .filter(Boolean)
+                .join('-'))
+            .join(' ');
         if (options && options.extra) {
-            return [result, options.extra].filter(Boolean).join(' ')
+            return [scoped, options.extra].filter(Boolean).join(' ')
         }
-        return result;
+        return scoped;
     }
 };
-export default scopeClass
+export default scopeClass;
