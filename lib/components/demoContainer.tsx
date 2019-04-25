46547443cba8f9465f7dec7e30c0c5aab314b455
shenfeng1945 <823902826@qq.com>
2019-04-26 05:40:01 +0800

    使用prism-react-renderer使源码高亮

--- lib/components/demoContainer.tsx
+++ lib/components/demoContainer.tsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Highlight, {defaultProps} from "prism-react-renderer";
+import Button from "./button/button";
 
 interface Props {
     code: string;
 }
+
 const DemoContainer: React.FunctionComponent<Props> = (props) => {
+    const [codeVisiable, setCodeVisiable] = useState(false);
+    const code = (
+        <Highlight {...defaultProps} code={props.code} language="jsx">
+            {({className, style, tokens, getLineProps, getTokenProps}) => (
+                <pre className={className} style={style}>
+                        {tokens.map((line, i) => (
+                            <div {...getLineProps({line, key: i})}>
+                                {line.map((token, key) => (
+                                    <span {...getTokenProps({token, key})} />
+                                ))}
+                            </div>
+                        ))}
+                    </pre>
+            )}
+        </Highlight>
+    );
     return (
         <div>
-           {props.children}
-           <pre>
-             {props.code}
-           </pre>
+            <div>
+                {props.children}
+            </div>
+            <Button onClick={() => setCodeVisiable(!codeVisiable)}>查看源码</Button>
+            {codeVisiable && code}
         </div>
     )
-}
+};
 export default DemoContainer;
